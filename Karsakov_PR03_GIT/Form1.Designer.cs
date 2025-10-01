3eb9c982d36b82ab9d422d02b584b1945f4c95a5
Karsakov13.07.2007 <roman.k-0777@mail.ru>
2025-10-01 17:17:04 +0300

    First Commit

--- Karsakov_PR03_GIT/Form1.Designer.cs
+++ Karsakov_PR03_GIT/Form1.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Karsakov";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
